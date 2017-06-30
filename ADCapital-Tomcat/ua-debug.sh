941eaec54653b9736b5402cf729a23cdca495a62
Mark Prichard <mqprichard@gmail.com>
2017-06-30 12:31:23 -0700

    Cleanup: remove old UA version files

--- ADCapital-Tomcat/ua-debug.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-source /env.sh
-
-# Set Universal Agent logging to DEBUG and refresh interval to 60 sec
-sed -i "s/    level: INFO/    level: DEBUG/g" /${UA_HOME}/conf/logging.yaml
-sed -i "s/#    interval: 300/    interval: 60/g" /${UA_HOME}/conf/universalagent.yaml
-service appdynamics-universal-agent restart
