ed502b01be9ba018b6544165608387e4a676d03d
Mark Prichard <mqprichard@gmail.com>
2017-07-20 22:44:19 +0100

    Added remote analytics agent support

--- ADCapital-AppDynamics/appdynamics.sh
+++ ADCapital-AppDynamics/appdynamics.sh
@@ -23,6 +23,7 @@ if [ -z "${APPD_TIER_NAME}" ]; then echo "APPD_TIER_NAME not defined" && exit; f
 APPD_RUNTIME_DIR="${APPD_DIR}/$(hostname)"
 mkdir -p ${APPD_RUNTIME_DIR}
 
+# Export environment variable with AppDynamics Agent properties
 export APPD_PROPERTIES="\
  -Dappdynamics.controller.hostName=${APPD_CONTR_HOST}\
  -Dappdynamics.controller.port=${APPD_CONTR_PORT}\
@@ -32,5 +33,6 @@ export APPD_PROPERTIES="\
  -Dappdynamics.agent.applicationName=${APPD_APP_NAME}\
  -Dappdynamics.agent.tierName=${APPD_TIER_NAME}\
  -Dappdynamics.agent.nodeName=${APPD_NODE_NAME}\
- -Dappdynamics.agent.runtime.dir=${APPD_RUNTIME_DIR}"
+ -Dappdynamics.agent.runtime.dir=${APPD_RUNTIME_DIR}\
+ -Dappdynamics.analytics.agent.url=http://monitor:9090/v2/sinks/bt"
 echo "APPD_PROPERTIES: ${APPD_PROPERTIES}"
