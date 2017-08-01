78b7782fd5be6c89b2517e86f9ce6cead22f3531
Mark Prichard <mqprichard@gmail.com>
2017-07-31 18:31:06 -0700

    Added Universal Agent

--- ADCapital-AppDynamics/startup.sh
+++ ADCapital-AppDynamics/startup.sh
@@ -1,17 +1,27 @@
 #! /bin/bash
 
-# Get agent version from zip file
-mkdir -p ${APPD_TMP}/appdynamics
-unzip -q ${AGENT_ZIP} -d ${APPD_TMP}/appdynamics
-
-APPD_VERSION=$(basename $(find ${APPD_TMP} -maxdepth 2 -type d -name "ver*")) 
-echo "App Server Agent Version: ${APPD_VERSION#ver}"
-
-# Create or replace per-version install directory
-rm -rf ${APPD_DIR}/${APPD_VERSION#ver} && mkdir -p ${APPD_DIR}/${APPD_VERSION#ver}
-cp -r ${APPD_TMP}/appdynamics/. ${APPD_DIR}/${APPD_VERSION#ver}/
-rm -rf ${APPD_TMP}/appdynamics
+echo "Configuring AppDynamics Universal Agent"
+sed -i "s/     controller_host: HOST/     controller_host: ${APPD_CONTR_HOST}/g" /${UA_HOME}/conf/universalagent.yaml
+sed -i "s/     controller_port: 1234/     controller_port: ${APPD_CONTR_PORT}/g" /${UA_HOME}/conf/universalagent.yaml
+sed -i "s/     account_name: ACCOUNT_NAME/     account_name: ${APPD_ACCOUNT_NAME%%_*}/g" /${UA_HOME}/conf/universalagent.yaml
+sed -i "s/     account_access_key: ACCESS_KEY/     account_access_key: ${APPD_ACCESS_KEY}/g" /${UA_HOME}/conf/universalagent.yaml
+sed -i "s/     global_account_name: GLOBAL_ACCOUNT_NAME/     global_account_name: ${APPD_GLOBAL_ACCOUNT_NAME}/g" /${UA_HOME}/conf/universalagent.yaml
 
 # Copy AppDynamics configuration script to shared volume
 echo "Copying AppDynamics configuration script to ${APPD_DIR}"
 /bin/cp -f appdynamics.sh ${APPD_DIR}/appdynamics.sh
+
+echo "Running AppDynamics Universal Agent..."
+${UA_HOME}/ua --daemon &
+
+sed -i "s/VERSION/${APPD_AGENT_VERSION}/g" ${UA_HOME}/rulebook/local.json
+
+AGENT_DOWNLOAD=$UA_HOME/download/monitor/java/${APPD_AGENT_VERSION}/java-${APPD_AGENT_VERSION}.zip
+AGENT_MONITOR=$UA_HOME/monitor/java/ver${APPD_AGENT_VERSION}
+
+# Test for UA to install to monitor folder to ensure zip download is complete
+until [ -e $AGENT_MONITOR ]; do sleep 1; done
+echo "Downloaded App Server Agent: ${APPD_AGENT_VERSION}"
+
+rm -rf ${APPD_DIR}/${APPD_AGENT_VERSION} && mkdir -p ${APPD_DIR}/${APPD_AGENT_VERSION}
+unzip -q ${AGENT_DOWNLOAD} -d ${APPD_DIR}/${APPD_AGENT_VERSION}
