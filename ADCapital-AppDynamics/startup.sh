fa20cf632cdfc8ea3fc32bee17c89c02c0617939
Mark Prichard <mqprichard@gmail.com>
2017-08-02 00:15:10 -0700

    Tidied up startup output messages

--- ADCapital-AppDynamics/startup.sh
+++ ADCapital-AppDynamics/startup.sh
@@ -6,16 +6,17 @@ MACHINE_AGENT_MONITOR=${UA_HOME}/monitor/machine/${APPD_MACHINE_AGENT_VERSION}/m
 ANALYTICS_AGENT_MONITOR=${MACHINE_AGENT_MONITOR}/monitors/analytics-agent
 
 configure-analytics(){
+  echo "Configuring Analytics Monitor"
   # Configure analytics-agent.properties
   aaprop=${ANALYTICS_AGENT_MONITOR}/conf/analytics-agent.properties
 
   if [ "${APPD_CONTR_SSL}" == "true" ]; then CONTROLLER_ENDPOINT="https:\/\/"; else CONTROLLER_ENDPOINT="http:\/\/"; fi
   CONTROLLER_ENDPOINT+="${APPD_CONTR_HOST}:${APPD_CONTR_PORT}"
-  echo "CONTROLLER_ENDPOINT: ${CONTROLLER_ENDPOINT//\\/}"
+  echo "Controller Endpoint: ${CONTROLLER_ENDPOINT//\\/}"
 
   if [ "${APPD_ES_SSL}" == "true" ]; then EVENT_ENDPOINT="https:\/\/"; else EVENT_ENDPOINT="http:\/\/"; fi
   EVENT_ENDPOINT+="${APPD_ES_HOST}:${APPD_ES_PORT}\/v1"
-  echo "EVENT_ENDPOINT: ${EVENT_ENDPOINT//\\/}"
+  echo "Events Endpoint: ${EVENT_ENDPOINT//\\/}"
 
   sed -i "/^ad.controller.url=/c\ad.controller.url=${CONTROLLER_ENDPOINT}" ${aaprop}
   sed -i "/^http.event.endpoint=/c\http.event.endpoint=${EVENT_ENDPOINT}" ${aaprop}
@@ -26,6 +27,7 @@ configure-analytics(){
   monxml=${ANALYTICS_AGENT_MONITOR}/monitor.xml
 
   sed -i 's#<enabled>false</enabled>#<enabled>true</enabled>#g' ${monxml}
+  echo "Analytics Monitor Enabled"
 }
 
 echo "Configuring AppDynamics Universal Agent"
@@ -35,7 +37,7 @@ sed -i "s/     account_name: ACCOUNT_NAME/     account_name: ${APPD_ACCOUNT_NAME
 sed -i "s/     account_access_key: ACCESS_KEY/     account_access_key: ${APPD_ACCESS_KEY}/g" /${UA_HOME}/conf/universalagent.yaml
 sed -i "s/     global_account_name: GLOBAL_ACCOUNT_NAME/     global_account_name: ${APPD_GLOBAL_ACCOUNT_NAME}/g" /${UA_HOME}/conf/universalagent.yaml
 
-echo "Running AppDynamics Universal Agent"
+echo "Starting AppDynamics Universal Agent"
 ${UA_HOME}/ua --daemon &
 
 # Configure Universal Agent local rulebook to download required App Server Agent version 
@@ -43,6 +45,7 @@ ${UA_HOME}/ua --daemon &
 
 # Test for UA to install to monitor folder to ensure zip download is complete
 until [ -e $AGENT_MONITOR ]; do sleep 1; done
+echo "Downloaded App Server Agent ${APPD_AGENT_VERSION}"
 
 echo "Copying App Server Agent ${APPD_AGENT_VERSION} to volume: ${APPD_DIR}"
 rm -rf ${APPD_DIR}/${APPD_AGENT_VERSION} && mkdir -p ${APPD_DIR}/${APPD_AGENT_VERSION}
@@ -53,10 +56,9 @@ echo "Copying AppDynamics configuration script to volume: ${APPD_DIR}"
 
 # Test for UA to install to monitor folder to ensure zip download is complete
 until [ -e ${ANALYTICS_AGENT_MONITOR}/conf/analytics-agent.properties ]; do sleep 1; done
-
 echo "Downloaded Machine Agent ${APPD_MACHINE_AGENT_VERSION}"
 
-echo "Stopping Universal Agent"
+echo "Stopping AppDynamics Universal Agent"
 ps -ef -ww | grep "ua --daemon" | grep -v "grep" | awk '{print $2}' | xargs kill
 
 # See the AppDynamics Product Docs (Standalone Machine Agent Configuration Reference)
@@ -77,7 +79,7 @@ if [ "${APPD_ANALYTICS_MONITOR}" == "true" ]; then
   configure-analytics
 fi
 
-echo "MA_PROPERTIES: ${MA_PROPERTIES}"
+echo "Starting AppDynamics Machine Agent with properties: ${MA_PROPERTIES}"
 
 # Start Machine Agent
 chmod +x ${MACHINE_AGENT_MONITOR}/jre/bin/java
