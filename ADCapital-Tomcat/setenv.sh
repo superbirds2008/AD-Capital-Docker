b2ee71bd55cfd99f69f66c4d0750d5bf05b4382b
ericjohanson <ejoha001@gmail.com>
2018-02-02 09:33:52 -0800

    final changes docker images

--- ADCapital-Tomcat/setenv.sh
+++ ADCapital-Tomcat/setenv.sh
@@ -1,3 +1,6 @@
+
 # Set AppDynamics Java Agent environment variables using docker run -e, --env or --env-file
 # https://docs.appdynamics.com/display/PRO43/Use+Environment+Variables+for+Java+Agent+Settings
-export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/opt/appdynamics/javaagent.jar ${APPDYNAMICS_NODE_PREFIX:+-Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${APPDYNAMICS_NODE_PREFIX} -Dappdynamics.analytics.agent.url=http://monitor:9090/v2/sinks/bt}"
+export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/opt/appdynamics/javaagent.jar ${APPDYNAMICS_NODE_PREFIX:+-Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${APPDYNAMICS_NODE_PREFIX}} -Dappdynamics.analytics.agent.url=http://monitor:9090/v2/sinks/bt"
+#export APPD_JAVAAGENT=" -javaagent:${APPD_DIR}/java-agent/javaagent.jar"
+#
--- ADCapital-Tomcat/startup.sh
+++ ADCapital-Tomcat/startup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Add AppDynamics Agent properties if shared volume is mounted
-# APPD_JAVAAGENT and APPD_PROPERTIES environment variables will be set
+#https://docs.appdynamics.com/display/PRO44/Installing+Agent-Side+Components#InstallingAgent-SideComponents-ConfiguretheAnalyticsAgentforaRemoteHost
+export APPD_ANALYTICS_AGENT="-Dappdynamics.analytics.agent.url=http://monitor:9090/v2/sinks/bt"
+export APPD_JAVAAGENT="-javaagent:/opt/appdynamics/javaagent.jar"
 
-if [ -e ${APPD_DIR}/appdynamics.sh ]; then
-  . ${APPD_DIR}/appdynamics.sh
-fi
-
-# Uncomment and expose port 8888 to enable JMX remote monitoring
-#export JMX_OPTS="-Dcom.sun.management.jmxremote.port=8888  -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 
 # Specialize container behavior based on ROLE env var
 # Uses https://github.com/jwilder/dockerize to check service dependencies
@@ -24,7 +19,7 @@ rest)
   cd ${CATALINA_HOME}/bin;
 
 
-  /docker-java-home/jre/bin/java ${APPD_JAVAAGENT} ${APPD_PROPERTIES} ${JMX_OPTS} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
+  /docker-java-home/jre/bin/java ${APPD_ANALYTICS_AGENT} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
   ;;
 portal)
   dockerize -wait tcp://rabbitmq:5672 \
@@ -33,7 +28,7 @@ portal)
 
   cp /${PROJECT}/AD-Capital/Portal/build/libs/portal.war ${CATALINA_HOME}/webapps;
   cd ${CATALINA_HOME}/bin;
-  /docker-java-home/jre/bin/java ${APPD_JAVAAGENT} ${APPD_PROPERTIES} ${JMX_OPTS} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
+  /docker-java-home/jre/bin/java ${APPD_ANALYTICS_AGENT} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
   ;;
 processor)
   dockerize -wait tcp://adcapitaldb:3306 \
@@ -42,16 +37,16 @@ processor)
             -wait-retry-interval ${RETRY} -timeout ${TIMEOUT} || exit $?
 
   cp /${PROJECT}/AD-Capital/Processor/build/libs/processor.war ${CATALINA_HOME}/webapps;
-  cd ${CATALINA_HOME}/bin;
-  /docker-java-home/jre/bin/java ${APPD_JAVAAGENT} ${APPD_PROPERTIES} ${JMX_OPTS} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
+
+
+  /docker-java-home/jre/bin/java ${APPD_ANALYTICS_AGENT} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
   ;;
 approval)
   dockerize -wait tcp://rabbitmq:5672 \
             -wait tcp://rest:8080 \
             -wait-retry-interval ${RETRY} -timeout ${TIMEOUT} || exit $?
 
-  cd ${CATALINA_HOME}/bin;
-  /docker-java-home/jre/bin/java ${APPD_JAVAAGENT} ${APPD_PROPERTIES} ${JMX_OPTS} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
+  /docker-java-home/jre/bin/java ${APPD_JAVAAGENT} ${APPD_ANALYTICS_AGENT} -jar ${PROJECT}/AD-Capital/QueueReader/build/libs/QueueReader.jar
   ;;
 verification)
   dockerize -wait tcp://adcapitaldb:3306 \
@@ -60,7 +55,7 @@ verification)
             -wait-retry-interval ${RETRY} -timeout ${TIMEOUT} || exit $?
 
   cd ${CATALINA_HOME}/bin;
-  /docker-java-home/jre/bin/java ${APPD_JAVAAGENT} ${APPD_PROPERTIES} ${JMX_OPTS} -Djava.util.logging.config.file=/usr/local/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -javaagent:/opt/appdynamics/javaagent.jar -classpath /usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/tomcat -Dcatalina.home=/usr/local/tomcat -Djava.io.tmpdir=/usr/local/tomcat/temp org.apache.catalina.startup.Bootstrap start
+  /docker-java-home/jre/bin/java ${APPD_JAVAAGENT} ${APPD_ANALYTICS_AGENT} -jar ${PROJECT}/AD-Capital/Verification/build/libs/Verification.jar
   ;;
 *)
   echo "ROLE missing: container will exit"; exit 1
